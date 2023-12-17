f956dc3a8f5beb545514434d6241606a4b7e1382
ryosuke-horie <ryosuke03140@gmial.com>
2023-12-17 19:15:25 +0900

    lintコマンドを実行

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2019,
+    project: "./tsconfig.json",
+    sourceType: "module",
+  },
+  rules: {
+    "@typescript-eslint/no-floating-promises": ["error"],
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        argsIgnorePattern: "^_",
+      },
+    ],
+  },
+};
